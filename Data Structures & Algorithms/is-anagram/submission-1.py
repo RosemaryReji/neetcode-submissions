a1e14a3c46357140c7610fed69086481890d6d08
RosemaryReji <rosecep24@gmail.com>
2026-05-19 11:57:42 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,10 @@
+#time complexity and space both O(n)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        countS,countT={},{}
+        for i in range(len(s)):
+            countS[s[i]]=countS.get(s[i],0)+1
+            countT[t[i]]=countT.get(t[i],0)+1
+        return countS==countT
